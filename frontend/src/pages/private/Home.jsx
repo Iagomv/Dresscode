d711be4984b762d19d0d7a4be56435bfde83a71b
Iago M. V. <iagomartinezv@gmail.com>
2025-06-01 14:13:57 +0200

    fix: role check in AuthContext

--- frontend/src/pages/private/Home.jsx
+++ frontend/src/pages/private/Home.jsx
@@ -1,31 +1,25 @@
 import React from "react";
 import { useAuth } from "../../context/AuthContext";
-import IncidentsVisualizationPage from "../user/IncidentsVisualizationPage";
-import TechnicianIncidentsPage from "../technician/TechnicianIncidentsPage";
 import LoadingSpinner from "../../components/common/LoadingSpinner";
-import IncidentAssignmentPage from "../admin/IncidentAssignmentPage";
 
 export default function Home() {
-  const { auth, isLoading } = useAuth();
+  const { auth } = useAuth();
 
-  const role = auth?.user?.authorities?.[0]?.replace("ROLE_", "") || null;
-
-  if (isLoading) {
-    return <LoadingSpinner />;
-  }
+  const role = auth?.user?.role?.replace("ROLE_", "") || null;
 
   if (!role) {
-    return <div>{ERROR_MESSAGES.noRoleError}</div>;
+    return <div>NO ROLE ERROR</div>;
   }
 
   switch (role.toUpperCase()) {
     case "STUDENT":
-      return <IncidentsVisualizationPage />;
+      return <div>STUDENT HOME</div>;
     case "TEACHER":
-      return <TechnicianIncidentsPage />;
+      return <div>TEACHER HOME</div>;
     case "ADMIN":
-      return <IncidentAssignmentPage />;
+      return <div>ADMIN HOME</div>;
+
     default:
-      return <div>{ERROR_MESSAGES.noRoleError}</div>;
+      return <div>UNKNOWN ERROR</div>;
   }
 }
