dc358ab0e2d29b4dbd131180adf97908312cb09a
Iago M. V. <iagomartinezv@gmail.com>
2025-06-07 20:36:48 +0200

    fix: remove console.log

--- frontend/src/components/public/login/useLoginForm.js
+++ frontend/src/components/public/login/useLoginForm.js
@@ -1,51 +1,50 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useFormik } from "formik";
-import {ApiConfig} from "../../../api/ApiConfig";
-import { useAuth } from "../../../context/AuthContext";
-import { getUserLoginSchema } from "../../../schema/UserLoginSchema";
-import { PATHS } from "../../../constants/routes";
-import { useTranslation } from "react-i18next";
-import { toast } from "react-toastify";
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useFormik } from 'formik'
+import { ApiConfig } from '../../../api/ApiConfig'
+import { useAuth } from '../../../context/AuthContext'
+import { getUserLoginSchema } from '../../../schema/UserLoginSchema'
+import { PATHS } from '../../../constants/routes'
+import { useTranslation } from 'react-i18next'
+import { toast } from 'react-toastify'
 
 export const useLoginForm = () => {
-  const navigate = useNavigate();
-  const { login } = useAuth();
-  const { t } = useTranslation("loginRegister");
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  
+  const navigate = useNavigate()
+  const { login } = useAuth()
+  const { t } = useTranslation('loginRegister')
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
   const handleLogin = async (response) => {
-    toast.success(t("loginSuccessful"));
-    console.log(response.token);
-    login(response.token);
+    toast.success(t('loginSuccessful'))
+    login(response.token)
     setTimeout(() => {
-      navigate(PATHS.dresscode.home, { replace: true });
-    }, 1000);
-  };
+      navigate(PATHS.dresscode.home, { replace: true })
+    }, 1000)
+  }
 
   const formik = useFormik({
     initialValues: {
-      email: "iagomartinezv@gmail.com",
-      password: "Passw0rd",
+      email: 'iagomartinezv@gmail.com',
+      password: 'Passw0rd',
     },
     validationSchema: getUserLoginSchema(t),
     onSubmit: async (values) => {
-      setIsSubmitting(true);
+      setIsSubmitting(true)
 
       try {
         const response = await ApiConfig.loginUser({
           email: values.email,
           password: values.password,
-        });
-        await handleLogin(response);
+        })
+        await handleLogin(response)
       } catch (error) {
-        console.error("Login error:", error);
-        toast.error(t("invalidCredentials"));
+        console.error('Login error:', error)
+        toast.error(t('invalidCredentials'))
       } finally {
-        setIsSubmitting(false);
+        setIsSubmitting(false)
       }
     },
-  });
+  })
 
-  return { formik, isSubmitting};
-};
+  return { formik, isSubmitting }
+}
