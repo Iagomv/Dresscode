f98b22edbdb9f3bd0e3d2b24ab45470f2435b0ea
Iago M. V. <iagomartinezv@gmail.com>
2025-06-08 13:28:00 +0200

    feat: add ClothingItem dtos and mapper

---
+++ backend/src/main/java/com/dresscode/dto/clothingItem/ClothingItemRequestDto.java
@@ -0,0 +1,39 @@
+package com.dresscode.dto.clothingItem;
+
+import com.dresscode.enums.*;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+@Data
+public class ClothingItemRequestDto {
+
+    @Size(min = 3, max = 100)
+    private String name;
+
+    @Size(max = 255)
+    private String imageUrl;
+
+    private String description;
+
+    private int quantity;
+
+    @Size(max = 50)
+    private String color;
+
+    private BigDecimal price;
+
+    private Timestamp acquisitionDate;
+
+    private ClothingItemStateEnum state;
+
+    private ClothingItemGenderEnum gender;
+
+    private ClothingItemTypeEnum type;
+
+    private ClothingItemSizeEnum size;
+
+    private ClothingItemAvailabilityEnum availability;
+
+}
