12bd4239bb3046b80f7c3edc1f777bbcb83d492d
Iago M. V. <iagomartinezv@gmail.com>
2025-06-12 16:04:34 +0200

    📦 feat(events): add EventRepository custom queries for stats data

--- backend/src/main/java/com/dresscode/repository/EventRepository.java
+++ backend/src/main/java/com/dresscode/repository/EventRepository.java
@@ -4,6 +4,8 @@ import java.util.Collection;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import com.dresscode.enums.EventCategoryEnum;
 import com.dresscode.enums.EventStatusEnum;
 import com.dresscode.model.Event;
@@ -15,4 +17,9 @@ public interface EventRepository extends JpaRepository<Event, Long> {
 
     List<Event> findByStatusNotIn(Collection<EventStatusEnum> statuses);
 
+    @Query("SELECT e.status, COUNT(e) FROM Event e GROUP BY e.status")
+    List<Object[]> countByStatus();
+
+    @Query("SELECT e.category, COUNT(e) FROM Event e GROUP BY e.category")
+    List<Object[]> countByCategory();
 }
